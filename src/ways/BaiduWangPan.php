b811a2d1a083f3b7ef6a2c5dc4ff3cbedcc8db5b
leftsky <leftsky@vip.qq.com>
2022-12-06 19:11:54 +0800

    增加获得百度授权链接

--- src/ways/BaiduWangPan.php
+++ src/ways/BaiduWangPan.php
@@ -5,4 +5,18 @@ namespace Leftsky\Storage\ways;
 class BaiduWangPan extends Base
 {
 
+    // 授权回调地址
+    private string $reurl = "";
+    // 百度网盘提供的三个密钥
+    private string $AppKey = "";
+    private string $SecretKey = "";
+    private string $SignKey = "";
+
+    public function getOathUrl(): string
+    {
+        return "http://openapi.baidu.com/oauth/2.0/authorize?"
+            . "response_type=code&client_id={$this->AppKey}&redirect_uri="
+            . "{$this->reurl}&scope=basic,netdisk";
+    }
+
 }
